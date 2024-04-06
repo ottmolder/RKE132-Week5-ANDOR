bd5cc87de7ada44abb0d9203c52819472f7c446d
Ott Mlder. <ott.molder@tktk.ee>
2024-04-06 18:21:58 +0300

    add week5 code

---
+++ ANDOperator/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your math result");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your biology result");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your chemistry result");
+chemistry = Int32.Parse(Console.ReadLine());
+
+
+if(math >= 90 && biology >= 90 && chemistry >= 90)
+{
+    Console.WriteLine("Congrats! You got acceepted");
+}
+else
+{
+    Console.WriteLine("Your applicatiopn cannot be approved");
+}
---
+++ OROperator/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your math result");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("enter your biology result");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your chemistry result");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if((math >= 90 && chemistry >= 90)  || (math >= 90 && biology >= 90) || (chemistry >= 90  && biology >= 90))
+{
+    Console.WriteLine("Congrats! you got accepted");
+}
+else
+{
+    Console.WriteLine("Your application cannot be approved");
+}
